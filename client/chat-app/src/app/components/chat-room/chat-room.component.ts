d488d2631188f6d4337bcb37797a602ab382056e
Ruslan Daumbaev <ruslan.daumbaev@gmail.com>
2020-10-04 01:33:07 +0300

    Styles changed

--- client/chat-app/src/app/components/chat-room/chat-room.component.ts
+++ client/chat-app/src/app/components/chat-room/chat-room.component.ts
@@ -14,6 +14,7 @@ import { NotificationsService } from 'src/app/services/notifications.service';
 const USERNAME_STORAGE_KEY = 'chat-app-user';
 
 @Component({
+  selector: 'app-chat-room',
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './chat-room.component.html',
 })
